7e6e3b5287de3cfc9779841676ae72134372d2a6
edarakchiev <e_darakchiev@yahoo.com>
2021-10-29 11:50:04 +0300

    add exercise

--- AsynchronousProgramming/lab/Github-Repos/scripts/app.js
+++ AsynchronousProgramming/lab/Github-Repos/scripts/app.js
@@ -1,3 +1,28 @@
 function loadRepos() {
-	console.log("TODO...");
+	const username = document.getElementById('username').value
+	const list = document.getElementById('repos')
+	const url = `https://api.github.com/users/${username}/repos`
+
+	fetch(url)
+		.then(res => {
+			if (res.ok === false){
+				throw new Error(`${res.status} - ${res.statusText}`)
+			}
+			return res.json()
+		})
+		.then(handleResponse)
+		.catch(handleError)
+
+	function handleResponse(data) {
+		list.innerHTML = ""
+		for (let repo of data){
+			let liEl = document.createElement('li')
+			liEl.innerHTML = `<a href="${repo.html_url}">${repo.full_name}</a>`
+			list.appendChild(liEl)
+		}
+	}
+	function handleError(error) {
+		list.innerHTML = ""
+		list.textContent = `${error.message}`
+	}
 }
