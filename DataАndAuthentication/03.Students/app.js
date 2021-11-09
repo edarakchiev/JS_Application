64a176934e746853e314d02be833124f9f5f389b
edarakchiev <e_darakchiev@yahoo.com>
2021-11-09 19:35:25 +0200

    Update app.js

--- DataАndAuthentication/03.Students/app.js
+++ DataАndAuthentication/03.Students/app.js
@@ -1,1 +1,55 @@
-console.log('TODO...');
+const resultTable = document.querySelector('#results tbody')
+const submitBtn = document.getElementById('submit')
+submitBtn.addEventListener('click', loadStudents)
+
+
+
+
+function createRow(firstName, lastName, facultyNumber, grade) {
+    const trElement = document.createElement('tr')
+    trElement.innerHTML = `<th>${firstName}</th>
+<th>${lastName}</th>
+<th>${facultyNumber}</th>
+<th>${grade}</th>`
+    return trElement
+}
+
+async function loadStudents(e){
+    e.preventDefault()
+    console.log('here')
+    let studentData = await getStudents()
+    console.log(studentData)
+    for (let el of studentData){
+        let row = createRow(el.firstName, el.lastName, el.facultyNumber, el.grade)
+        resultTable.appendChild(row)
+    }
+}
+
+async function getStudents() {
+    const url = 'http://localhost:3030/jsonstore/collections/students'
+    const response = await fetch(url)
+    const data = await response.json()
+    return Object.values(data)
+}
+
+async function createStudent() {
+    const url = 'http://localhost:3030/jsonstore/collections/students'
+    const options = {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+    try {
+        const response = await fetch(url, options)
+        if (response.ok === false) {
+            throw new Error(response.statusText)
+        }
+        const data = await response.json()
+        return data
+    }catch (err){
+        alert(err.message)
+    }
+}
+
