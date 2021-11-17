66bcffc1058b474b11bf4f4aac97f666f63ff05f
edarakchiev <e_darakchiev@yahoo.com>
2021-11-17 18:09:56 +0200

    update

--- ArchitectureAndTesting/SoftTerest/src/app.js
+++ ArchitectureAndTesting/SoftTerest/src/app.js
@@ -1,2 +1,47 @@
-import * as api from './api/data.js';
-window.api = api
+import { showSection } from "./views/dom.js"
+import { showCatalogPage } from "./views/catalog.js"
+import { showCreatePage } from "./views/create.js"
+import { showDetailsPage } from "./views/details.js"
+import { showHomePage } from "./views/home.js"
+import { showLoginPage } from "./views/login.js"
+import { showRegisterPage } from "./views/register.js"
+
+const links = {
+    'homeLink': 'home',
+    'getStartedLink': 'home',
+    'catalogLink': 'catalog',
+    'loginLink': 'login',
+    'registerLink': 'register',
+    'createLink': 'create'
+}
+
+const views = {
+    'home': showHomePage,
+    'catalog': showCatalogPage,
+    'login': showLoginPage,
+    'register': showRegisterPage,
+    'create': showCreatePage,
+    'details': showDetailsPage
+}
+
+const nav = document.querySelector('nav');
+nav.addEventListener('click', onNavigate);
+const ctx = {
+    goTo,
+    showSection
+}
+
+function onNavigate(event) {
+    const name = links[event.target.id];
+    if (name) {
+        event.preventDefault();
+        goTo(name);
+    }
+}
+
+function goTo(name, ...params) {
+    const view = views[name];
+    if (typeof view == 'function'){
+        view(ctx, ...params);
+    }
+}
