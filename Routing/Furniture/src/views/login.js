cb6293e93ed8822ff53a85301eef91b743670919
edarakchiev <e_darakchiev@yahoo.com>
2021-11-25 07:48:15 +0200

    update

--- Routing/Furniture/src/views/login.js
+++ Routing/Furniture/src/views/login.js
@@ -1,7 +1,7 @@
 import { login } from "../api/api.js";
 import { html } from "../lib.js";
 
-const loginTemplate = (onSubmit) => html`
+const loginTemplate = (onSubmit, errMsg) => html`
 <div class="row space-top">
             <div class="col-md-12">
                 <h1>Login User</h1>
@@ -11,13 +11,14 @@ const loginTemplate = (onSubmit) => html`
         <form @submit=${onSubmit}>
             <div class="row space-top">
                 <div class="col-md-4">
+                    ${errMsg ? html`<div class="form-group error">${errMsg}</div>` : null}
                     <div class="form-group">
                         <label class="form-control-label" for="email">Email</label>
-                        <input class="form-control" id="email" type="text" name="email">
+                        <input class=${'form-control' + (errMsg ? ' is-invalid': '')} id="email" type="text" name="email">
                     </div>
                     <div class="form-group">
                         <label class="form-control-label" for="password">Password</label>
-                        <input class="form-control" id="password" type="password" name="password">
+                        <input class=${'form-control' + (errMsg ? ' is-invalid': '')} id="password" type="password" name="password">
                     </div>
                     <input type="submit" class="btn btn-primary" value="Login" />
                 </div>
@@ -25,17 +26,26 @@ const loginTemplate = (onSubmit) => html`
         </form>`
 
 export function loginPage(ctx) {
-    ctx.render(loginTemplate(onSubmit))
+    update()
+
+    function update(errorMsg){
+        ctx.render(loginTemplate(onSubmit, errorMsg))
+    }
+    
 
     async function onSubmit(event) {
         event.preventDefault()
         const formData = new FormData(event.target)
 
-        const email = formData.get('email')
-        const password = formData.get('password')
-
-        await login(email, password)
+        const email = formData.get('email').trim()
+        const password = formData.get('password').trim()
+        try{
+            await login(email, password)
         ctx.updateUserNav()
         ctx.page.redirect('/')
+        } catch(err) {
+            update(err.message)
+        }
+        
     }
 }
--- Routing/Furniture/src/views/register.js
+++ Routing/Furniture/src/views/register.js
@@ -1,5 +1,67 @@
+import { register } from "../api/api.js";
 import { html } from "../lib.js";
 
-export function registerPage() {
-    console.log('register page')
+
+const registerTemplate = (onSubmit, errMsg) => html`
+<div class="row space-top">
+            <div class="col-md-12">
+                <h1>Register New User</h1>
+                <p>Please fill all fields.</p>
+            </div>
+        </div>
+        <form @submit=${onSubmit}>
+            <div class="row space-top">
+                <div class="col-md-4">
+                ${errMsg ? html`<div class="form-group error">${errMsg}</div>` : null}
+                    <div class="form-group">
+                        <label class="form-control-label" for="email">Email</label>
+                        <input class="form-control" id="email" type="text" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="password">Password</label>
+                        <input class="form-control" id="password" type="password" name="password">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="rePass">Repeat</label>
+                        <input class="form-control" id="rePass" type="password" name="rePass">
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Register" />
+                </div>
+            </div>
+        </form>`
+
+
+export function registerPage(ctx) {
+    update()
+
+    function update(errorMsg){
+        ctx.render(registerTemplate(onSubmit, errorMsg))
+    }
+    
+
+    async function onSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.target)
+
+        const email = formData.get('email').trim()
+        const password = formData.get('password').trim()
+        const rePass = formData.get('rePass').trim()
+
+        try{
+            if (email == '' || password == ''){
+                throw new Error('All fields are required!')
+            }
+            if ( password != rePass) {
+                throw new Error('Password don\'t match!')
+            }
+    
+            await register(email, password)
+            ctx.updateUserNav()
+            ctx.page.redirect('/')
+        } catch(err) {
+            update(err.message)
+        }
+
+        
+    }
 }
